0677fcdec4a3a21850d252f739954470d31e9ff0
Wei Zizhen <weizizhen@idr.ai>
2023-04-06 15:02:55 +0800

    feat: animate

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
 <template>
-  <main h-screen font-serif bg-black p="x-4 y-10" text="center gray-200">
+  <main h-screen font-serif bg-black flex="~ col" text="center gray-200">
+    <div h-10 w-full class="line" style="--i:-1" />
     <RouterView />
+    <div h-10 w-full class="line" style="--i:1" />
   </main>
 </template>
+
+<style>
+.line {
+  background: linear-gradient(45deg, #27272A 10%, transparent 10%,
+    transparent 50%, #27272A 50%, #27272A 60%, transparent 60%, transparent 100%);
+  background-size: 40px 40px;
+  animation: scroll 0.5s linear infinite;
+}
+
+@keyframes scroll {
+  0% {
+    background-position: 0;
+  }
+  100% {
+    background-position: calc(var(--i) * 40px);
+  }
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,6 +3,12 @@ defineOptions({
   name: 'IndexPage',
 })
 
+interface Hitokoto {
+  id: number
+  hitokoto: string
+  from: string
+}
+
 let isCopyDone = $ref(false)
 
 const api = 'https://v1.hitokoto.cn'
@@ -11,9 +17,24 @@ const urlParams = new URLSearchParams({
   encode: 'json',
 })
 
-const { data, execute } = useFetch(`${api}?${urlParams}`).get().json()
+const { data, execute } = useFetch(`${api}?${urlParams}`).get().json<Hitokoto>()
+
+const reference = $computed(() => {
+  if (data.value?.from) {
+    if (data.value.from.includes('·')) {
+      const [left, right] = data.value.from.split('·')
+      return `《${left} · ${right}》`
+    }
+    else { return `《${data.value.from}》` }
+  }
+
+  return ''
+})
 
 function onClick() {
+  if (!data.value)
+    return
+
   navigator.clipboard.writeText(data.value.hitokoto)
 
   if (!isCopyDone) {
@@ -23,8 +44,6 @@ function onClick() {
     }, 1800)
   }
 }
-
-document.addEventListener('wheel', () => console.log('asd'))
 </script>
 
 <template>
@@ -34,7 +53,7 @@ document.addEventListener('wheel', () => console.log('asd'))
         w-auto px-4 py-0.5 cursor-pointer rounded-md
         lg:text-14 md:text-10 sm:text-8
         transition transition-all duration-300
-        class="hover:bg-coolGray/20"
+        class="hover:bg-zinc2/20"
         @click="onClick"
       >
         <Transition name="fit" mode="out-in">
@@ -49,16 +68,16 @@ document.addEventListener('wheel', () => console.log('asd'))
             </TheSentence>
           </div>
           <template v-else>
-            <div i-carbon-ai-status-complete class="w-10 h-10 inline-block" />
+            <div i-carbon-ai-status-complete class="lg:w-10 lg:h-10 sm:w-5 sm:h-5 inline-block" />
           </template>
         </Transition>
       </div>
 
       <div my-2 />
 
-      <div lg:text-6 text-4 class="text-amber/70">
+      <div lg:text-6 text-4 class="text-amber/70 reflex">
         <TheSentence>
-          <span tracking-widest class="sentence">{{ `《${data?.from ?? ''}》` }}</span>
+          <span tracking-widest class="sentence reflex">{{ reference }}</span>
         </TheSentence>
       </div>
     </div>
@@ -79,5 +98,9 @@ document.addEventListener('wheel', () => console.log('asd'))
 .fit-leave-to {
   opacity: 0;
   transform: scale(0.7);
+}
+
+.reflex {
+  -webkit-box-reflect: below 2px linear-gradient(transparent, #0004);
 }
 </style>
